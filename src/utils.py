07860d322556c788a121b617d531d90aa88dba30
ktylus <karaxek23@gmail.com>
2024-12-10 18:37:04 +0100

    get iupac name of smiles

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,10 @@
+import copy
+
 import matplotlib.pyplot as plt
+import pubchempy as pcp
 from sklearn.tree import DecisionTreeRegressor, plot_tree
 from rdkit import Chem
-from rdkit.Chem import AllChem, Draw
+from rdkit.Chem import AllChem, Draw, rdMolDescriptors
 
 
 def save_decision_tree_graph(model: DecisionTreeRegressor, filename: str):
@@ -29,10 +32,14 @@ def get_data_target_field_names():
 def draw_morgan_bit_many_molecules(molecules: list, bit_id, radius=2, length=1024):
     target_mols = []
     bit_infos = []
+    mols_found = 0
     for i, mol in enumerate(molecules):
+        if mols_found == 10:
+            break
         bit_info = {}
         fp = AllChem.GetMorganFingerprintAsBitVect(mol, nBits=length, radius=radius, bitInfo=bit_info)
         if fp[bit_id]:
+            mols_found += 1
             target_mols.append(mol)
             bit_infos.append(bit_info)
     imgs = []
@@ -50,3 +57,60 @@ def load_synthetic_target_data():
         smiles = f.readlines()
     molecules = [Chem.MolFromSmiles(smi) for smi in smiles]
     return molecules
+
+
+def get_iupac_name_of_smiles(smiles_string):
+    """
+    Returns the IUPAC name of a compound given its SMILES string.
+
+    Parameters:
+    smiles_string (str): The SMILES string of the compound.
+
+    Returns:
+    str: The IUPAC name of the compound.
+    """
+    # Search PubChem for the compound.
+    compound = pcp.get_compounds(smiles_string, 'smiles')
+    iupac_name = compound[0].iupac_name if compound else 'Not found'
+    return iupac_name
+
+
+def get_fingerprint_with_zeroed_top_bits(X, top_bits):
+    X_inverted = copy.deepcopy(X)
+    X_inverted[:, top_bits] = 0
+    return X_inverted
+
+
+def get_morgan_fragment(molecule, bit, radius=2, nBits=2048):
+    """
+    Returns the molecule fragment corresponding to a given Morgan fingerprint bit.
+
+    Parameters:
+    molecule (rdkit.Chem.rdchem.Mol): The molecule to analyze.
+    bit (int): The bit of the Morgan fingerprint.
+    radius (int): The radius of the Morgan fingerprint. Default is 2.
+    nBits (int): The size of the Morgan fingerprint. Default is 2048.
+
+    Returns:
+    rdkit.Chem.rdchem.Mol: The fragment corresponding to the given bit.
+    """
+    # Generate the Morgan fingerprint
+    info = {}
+    _ = rdMolDescriptors.GetMorganFingerprintAsBitVect(molecule, radius, nBits=nBits, bitInfo=info)
+    
+    # Check if the bit is in the fingerprint
+    if bit not in info:
+        raise ValueError(f"Bit {bit} is not present in the fingerprint.")
+    
+    # Get the atom indices and radius for the bit
+    atom_indices, bit_radius = info[bit][0]
+    
+    # Get the fragment corresponding to the bit
+    if bit_radius == 0:
+        frag = Chem.MolFromSmarts(molecule.GetAtomWithIdx(atom_indices).GetSmarts())
+    else:
+        env = Chem.FindAtomEnvironmentOfRadiusN(molecule, bit_radius, atom_indices)
+        amap = {}
+        frag = Chem.PathToSubmol(molecule, env, atomMap=amap)
+    
+    return frag
