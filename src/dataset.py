33bd48a5154fa1eb55dd8be675b56284cb440c2c
ktylus <karaxek23@gmail.com>
2024-12-10 18:37:22 +0100

    function for synthetic dataset creation

--- src/dataset.py
+++ src/dataset.py
@@ -5,12 +5,12 @@ import numpy as np
 import torch
 
 
-def create_synthetic_dataset(n_samples=10000):
+def create_synthetic_dataset(smiles_for_class_task, n_samples=10000):
     with open("data/ChEMBL_filtered.txt", "r") as f:
         smiles = [next(f).strip() for _ in range(n_samples)]
     molecules = [Chem.MolFromSmiles(smi) for smi in smiles]
     # Set target values.
-    targets = create_synthetic_target(molecules, {"c1ccccc1": (1, 0)}, default_mean=0.0, default_std=0.0)
+    targets = create_synthetic_target(molecules, smiles_for_class_task)
     for i, mol in enumerate(molecules):
         mol.SetProp("y", str(targets[i]))
     return molecules
@@ -18,19 +18,13 @@ def create_synthetic_dataset(n_samples=10000):
 
 def create_synthetic_target(
         data,
-        mol_mean_and_std,
-        default_mean=0.0,
-        default_std=1.0
+        smiles_for_class_task,
 ):
     y_synthetic = []
     for mol in data:
-        target_value = 0.0
-        for mol_to_check in mol_mean_and_std:
-            if mol.HasSubstructMatch(Chem.MolFromSmarts(mol_to_check)):
-                target_value += np.random.normal(loc=mol_mean_and_std[mol_to_check][0],
-                                                 scale=mol_mean_and_std[mol_to_check][1])
-        if target_value == 0.0:
-            target_value = np.random.normal(loc=default_mean, scale=default_std)
+        target_value = 0
+        if mol.HasSubstructMatch(Chem.MolFromSmiles(smiles_for_class_task)):
+            target_value = 1
         y_synthetic.append(target_value)
     return np.array(y_synthetic)
 
