23ca2efdcaa962ce1d265049480a4909d8223cd6
ktylus <karaxek23@gmail.com>
2025-02-09 10:15:43 +0100

    refactor

--- src/metrics.py
+++ src/metrics.py
@@ -2,7 +2,10 @@ from src.utils import min_max_scale_graph_batch
 
 
 def calculate_batch_explanation_contrastivity(
-        explanation_batch, explanation_batch_inv, node_assignments, threshold=0.1
+        explanation_batch,
+        explanation_batch_inv,
+        node_assignments,
+        threshold=0.1
 ):
     exp_batch_scaled = min_max_scale_graph_batch(explanation_batch, node_assignments)
     exp_batch_inv_scaled = min_max_scale_graph_batch(explanation_batch_inv, node_assignments)
@@ -13,6 +16,10 @@ def calculate_batch_explanation_contrastivity(
     return exp_xor.sum().item() / total_nodes_found
 
 
-def calculate_batch_explanation_sparsity(explanation_batch, node_assignments, threshold=0.1):
+def calculate_batch_explanation_sparsity(
+        explanation_batch,
+        node_assignments,
+        threshold=0.1
+):
     exp_batch_scaled = min_max_scale_graph_batch(explanation_batch, node_assignments)
     return 1 - ((exp_batch_scaled > threshold).sum() / exp_batch_scaled.numel()).item()
